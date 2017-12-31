f226e918adaeeffd1e6d767872b99b0acead6614
krworks <kyle@krworks.com>
2017-12-30 23:08:24 -0700

    Add Data class with Base64 serialization

--- obj/data.h
+++ obj/data.h
@@ -2,8 +2,11 @@
 #define _DATA_
 
 #define _Data(D,T,C) _Base(spr,T,C)        \
+    override(D,T,C,void,free,(C))          \
     override(D,T,C,String,to_string,(C))   \
-    override(D,T,C,C,from_string,(String))
-declare(Enum, Base);
+    override(D,T,C,C,from_string,(String)) \
+    var(D,T,C,uint8 *,bytes)               \
+    var(D,T,C,uint,length)
+declare(Data, Base);
 
 #endif
