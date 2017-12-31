e38274d6285fe1cb7de3d2e5921e13c6cb1c534d
krworks <kyle@krworks.com>
2017-12-31 01:12:11 -0700

    Integrate stb zlib algorithm into Data serialization

--- obj/data.c
+++ obj/data.c
@@ -1,28 +1,42 @@
 #include <obj/obj.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STBI_NO_BMP
+#define STBI_NO_PSD
+#define STBI_NO_TGA
+#define STBI_NO_GIF
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+#include <obj/stb_image.h>
+#include <obj/stb_image_write.h>
+
 implement(Data)
 
 String Data_to_string(Data self) {
-    const char *b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	const char pad = '=';
+    const uint8 *b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	const uint8 pad = '=';
 	String str = auto(String);
-    int divis = self->length / 3 * 3;
-
+    int clength = 0;
+    uint8 *cbytes = stbi_zlib_compress(self->bytes, self->length, &clength, 0);
+    int divis = clength / 3 * 3;
+    
     for (int x = 0; x < divis; x += 3) {
-        char b0 = self->bytes[x], b1 = self->bytes[x + 1], b2 = self->bytes[x + 2];
-        char buf[4];
+        uint8 b0 = cbytes[x], b1 = cbytes[x + 1], b2 = cbytes[x + 2];
+        uint8 buf[4];
         buf[0] = b64[b0 >> 2];
         buf[1] = b64[((b0 & 0x03) << 4) | ((b1 & 0xF0) >> 4)];
-        buf[2] = b64[((b1 & 0x0F) << 2) | ((b2 & 0x70) >> 6)];
+        buf[2] = b64[((b1 & 0x0F) << 2) | (b2 >> 6)];
         buf[3] = b64[b2 & 0x3F];
         call(str, concat_chars, buf, 4);
     }
-    int remaining = self->length - divis;
+    int remaining = clength - divis;
     if (remaining > 0) {
-        char buf[4];
-        char b0 = self->bytes[divis];
+        uint8 buf[4];
+        uint8 b0 = cbytes[divis];
         if (remaining == 2) {
-            char b1 = self->bytes[divis + 1];
+            uint8 b1 = cbytes[divis + 1];
             buf[0] = b64[b0 >> 2];
             buf[1] = b64[((b0 & 0x03) << 4) | ((b1 & 0xF0) >> 4)];
             buf[2] = b64[((b1 & 0x0F) << 2)];
@@ -33,7 +47,7 @@ String Data_to_string(Data self) {
             buf[2] = pad;
             buf[3] = pad;
         }
-        call(str, concat_chars, buf, 4);
+        call(str, concat_chars, (const char *)buf, 4);
     }
     return str;
 }
@@ -43,58 +57,62 @@ Data Data_from_string(String value) {
         return NULL;
     size_t alloc_len = (size_t)value->length / 4 * 3;
     Data self = auto(Data);
-    self->bytes = alloc_bytes(alloc_len);
+    uint8 *cbytes = alloc_bytes(alloc_len);
     int cursor = 0;
     int pfound = 0;
 
-    const static unsigned char map[] = {
-        0, 0, 0, 62, 0, 0, 0, 63, 52, 53,       // 50
-        54, 55, 56, 57, 58, 59, 60, 61, 0, 0,   // 60
-        0, 0, 0, 0, 0, 0, 1, 2, 3, 4,           // 70
+    const static int16 map[] = {
+        -1, -1, -1, 62, -1, -1, -1, 63, 52, 53,       // 50
+        54, 55, 56, 57, 58, 59, 60, 61, -1, -1,   // 60
+        -1, -1, -1, -1, -1, 0, 1, 2, 3, 4,           // 70
         5, 6, 7, 8, 9, 10, 11, 12, 13, 14,      // 80
         15, 16, 17, 18, 19, 20, 21, 22, 23, 24, // 90
-        25, 0, 0, 0, 0, 0, 0, 26, 27, 28,       // 100
+        25, -1, -1, -1, -1, -1, -1, 26, 27, 28,       // 100
         29, 30, 31, 32, 33, 34, 35, 36, 37, 38, // 110
         39, 40, 41, 42, 43, 44, 45, 46, 47, 48, // 120
-        49, 50, 51, 0, 0, 0, 0, 0, 0, 0         // 130
+        49, 50, 51, -1, -1, -1, -1, -1, -1, -1         // 130
     };
 
     for (int i = 0; i < value->length; i += 4) {
-        char x[4];
+        uint8 x[4];
         for (int ii = 0; ii < 4; ii++) {
-            char b = value->buffer[i + ii];
+            uint8 b = value->buffer[i + ii];
             if (b < 40 || b >= 130)
                 return NULL;
-            char v;
+            int16 v;
             if (b == '=') {
                 v = 0;
                 pfound++;
             } else {
                 v = map[b - 40];
-                if (v == 0)
+                if (v == -1)
                     return NULL;
             }
-            x[ii] = v;
+            x[ii] = (uint8)v;
         }
-        char bytes[3];
+        uint8 bytes[3];
         bytes[0] = (x[0] << 2) | (x[1] >> 4);
         bytes[1] = ((x[1] & 0xF) << 4) | (x[2] >> 2);
         bytes[2] = (x[2] << 6) | (x[3] & 0x3F);
 
-        memcpy(&self->bytes[cursor], bytes, 3);
+        memcpy(&cbytes[cursor], bytes, 3);
         cursor += 3;
     }
     int pad = 0;
-    self->length = alloc_len;
+    int clength = alloc_len;
     if (value->length >= 4) {
         if (value->buffer[value->length - 1] == '=') {
             pad++;
             if (value->buffer[value->length - 2] == '=')
                 pad++;
         }
-        self->length -= pad;
+        clength -= pad;
     }
-    if (pfound != pad)
+
+    self->bytes = stbi_zlib_decode_malloc(cbytes, clength, (int *)&self->length);
+    free_ptr(cbytes);
+
+    if (!self->bytes || pfound != pad)
         return NULL;
     return self;
 }
