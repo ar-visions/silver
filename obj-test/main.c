485364c4e466db16fb403c1a5c1d3cda51701526
krworks <kyle@krworks.com>
2017-12-31 01:12:11 -0700

    Integrate stb zlib algorithm into Data serialization

--- obj-test/main.c
+++ obj-test/main.c
@@ -13,6 +13,24 @@ int main() {
     Boolean meta = prop_meta(test_list, "min_block_size", "test", Boolean);
     print(test_list, "list meta = %p", meta);
 
+    char *test_bytes = "leasure. leasure leasure leasure leasure leasure leasure leasure leasure leasure leasure";
+    Data data = new(Data);
+    data->bytes = (uint8 *)test_bytes;
+    data->length = strlen(test_bytes);
+    String data_str = call(data, to_string);
+
+    printf("data string: %s\n", data_str->buffer);
+
+    Data leasure_data = class_call(Data, from_string, data_str);
+    
+    printf("leasure_data: %d %d %d\n", leasure_data->bytes[0], leasure_data->bytes[1], leasure_data->bytes[2]);
+
+    String test_str = call(leasure_data, to_string);
+
+    printf("test string: %s\n", test_str->buffer);
+
+    printf("leasure: %s\n", leasure_data->bytes);
+
     List containing_meta = props_with_meta("test", List);
 
     list_push(v2->list, t);
