17262b217951f089a54ffb8fba3100f3fa975ea5
Kyle White <kyle.white@guardvant.com>
2018-01-03 17:22:36 -0700

    Integrate header processor into build system

--- obj-header/header.c
+++ obj-header/header.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include "llist.h"
 
 #define HEADER_VERSION "0.9.0"
 
@@ -206,6 +207,97 @@ char *delimit_quotes(char *in) {
     return out;
 }
 
+char *trim(char *in) {
+    int len = strlen(in);
+    int start = -1, end = -1;
+    for (int i = 0; i < len; i++) {
+        if (!isspace(in[i])) {
+            start = i;
+            for (int ii = len - 1; ii >= i; ii--) {
+                if (!isspace(in[ii])) {
+                    end = ii;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    char *ret;
+    if (start == -1 || end == -1) {
+        ret = (char *)malloc(1);
+        ret[0] = 0;
+    } else {
+        int size = end - start + 1;
+        ret = (char *)malloc(size + 1);
+        memcpy(ret, &in[start], size);
+        ret[size] = 0;
+    }
+    return ret;
+}
+
+
+static LList forwards;
+
+bool is_forward(char *type) {
+    if (forwards.count == 0)
+        return false;
+    for (type; *type && isspace(*type); type++) { };
+    int len = strlen(type);
+    char *word = (char *)malloc(len + 1);
+    int n = 0;
+    char *non_const = NULL;
+    while (sscanf(type, "%s%n", word, &n) == 1) {
+        if (strcmp(word, "const") != 0) {
+            non_const = copy_string(word);
+        } else if (strcmp(word, "struct") == 0 || strcmp(word, "enum") == 0) {
+            non_const = NULL;
+            break;
+        }
+        type = &type[n];
+        if (type[0] == 0)
+            break;
+    }
+    free(word);
+    free(non_const);
+}
+
+bool read_forwards(char *line, int *bytes_ahead) {
+    printf("line: %s\n", line);
+    char *origin = line;
+    for (line; *line && isspace(*line); line++) { };
+    int len = strlen(line);
+    char *word = (char *)malloc(len + 1);
+    int n = 0;
+    bool ret = false;
+    *bytes_ahead = 0;
+    if (sscanf(line, "%s%n", word, &n) == 1) {
+        if (strcmp(word, "forward") == 0) {
+            bool found_semi = false;
+            char *line2 = copy_to(line, ";", &found_semi, NULL);
+            char *orig = line2;
+            if (found_semi) {
+                *bytes_ahead = strlen(line2) + 1 + (int)((size_t)(line - origin));
+                line2 = &line2[n];
+                for (line2; *line2 && isspace(*line2); line2++) { };
+                char *pch = strtok(line2, " ,\r\n\t");
+                int valid_forwards = 0;
+                printf("pch = %s\n", pch);
+                while (pch != NULL) {
+                    if (pch[0] && !isspace(pch[0])) {
+                        printf("forward: %s\n", pch);
+                        valid_forwards++;
+                    }
+                    pch = strtok(NULL, " ,\r\n\t");
+                }
+                ret = valid_forwards > 0;
+            }
+            free(orig);
+        }
+    }
+    free(word);
+    return ret;
+}
+
 void main(int argc, char *argv[]) {
     if (argc != 3) {
         printf("libobj header preprocessor -- version %s\n", HEADER_VERSION);
@@ -242,13 +334,16 @@ void main(int argc, char *argv[]) {
     in[file_len] = 0;
     char *buf = (char *)malloc(file_len + 1);
     buf[0] = 0;
+    buf[file_len] = 0;
     // file input line by line
     char *line_start = in;
     bool rn = false;
     int block_len = 1;
 
     fprintf(fout, "#ifndef %s\n#define %s\n\n", file_def_str, file_def_str);
-
+    bool was_space = true;
+    bool was_new_line = true;
+    bool in_define = false;
     for (int i = 0; i < (int)file_len; i += block_len) {
         block_len = 1;
         char c = in[i];
@@ -258,7 +353,20 @@ void main(int argc, char *argv[]) {
         char *start = &in[i];
         int stop_at = 0;
         bool token_read = sscanf(start, "%s%n", buf, &stop_at) == 1;
-        if (!new_line && token_read && strcmp(buf, "class") == 0) {
+
+        if (new_line && in_define) {
+            in_define = i > 0 && (in[i - 1] == '\\');
+        }
+        if (was_new_line && token_read && strcmp(buf, "#define") == 0) {
+            fprintf(fout, "%c", c);
+            in_define = true;
+            was_space = false;
+        } else if (!in_define && token_read && strcmp(buf, "forward") == 0) {
+            if (!read_forwards(start, &block_len)) {
+                fprintf(stderr, "syntax error on forward\n");
+                exit(1);
+            }
+        } else if (!in_define && was_space && !new_line && token_read && strcmp(buf, "class") == 0) {
             int len = strlen(start);
             int index = 0;
             char *cname = (char *)malloc(len + 1);
@@ -295,8 +403,12 @@ void main(int argc, char *argv[]) {
                 fprintf(stderr, "invalid class declaration block\n");
                 exit_code(1);
             }
-            fprintf(fout, "#define _%s(D,T,C) _%s(spr,T,C) \\\n",
-                        cname, sname != 0 ? sname : (char *)"Base");
+            if (strcmp(cname, "Base") == 0) {
+                fprintf(fout, "#define _%s(D,T,C) \\\n", cname);
+            } else {
+                fprintf(fout, "#define _%s(D,T,C) _%s(spr,T,C) \\\n",
+                            cname, *sname != 0 ? sname : (char *)"Base");
+            }
 
             for (int e = 0; e < expr_count; e++) {
                 cur = expr[e];
@@ -318,24 +430,53 @@ void main(int argc, char *argv[]) {
                     free(hash);
                 }
                 if (sscanf(cur, "%s%n", token, &index) == 1) {
-                    cur = &cur[index];
-                    bool override = false;
+                    bool override = strcmp(token, "override") == 0;
                     bool delegate = strcmp(token, "delegate") == 0;
-                    if (strcmp(token, "override") == 0 || delegate) {
-                        override = !delegate;
-                        if (sscanf(cur, "%s%n", token, &index) != 1) {
-                            fprintf(stderr, "expected type after %s\n", token);
-                            exit_code(1);
-                        }
-                        cur += index;
-                    }
+                    bool private  = strcmp(token, "private")  == 0;
+                    bool ptr = false;
+                    if (override || delegate || private)
+                        cur = &cur[index];
                     for (cur; isspace(*cur); ++cur) { }
                     bool parens = false;
-                    char *type = copy_to(token, NULL, NULL, NULL);
-                    char *name = copy_to(cur, "(", &parens, NULL);
+                    
+                    char *whole = copy_to(cur, "(", &parens, NULL);
                     if (!parens) {
-                        free(name);
-                        name = copy_to(cur, ";", NULL, NULL);
+                        free(whole);
+                        whole = copy_to(cur, ";", NULL, NULL);
+                    }
+                    char *name = NULL;
+                    char *type = NULL;
+                    for (int z = strlen(whole) - 1; z > 0; z--) {
+                        char ch = whole[z];
+                        if (!isspace(ch)) {
+                            // end of name is at z; find beginning, and end of type
+                            for (int zz = z - 1; zz > 0; zz--) {
+                                char ch = whole[zz];
+                                if (isspace(ch) || ch == '*') {
+                                    bool proceed = false;
+                                    for (int zzz = zz - 1; zzz >= 0; zzz--) {
+                                        char ch = whole[zzz];
+                                        if (!isspace(ch)) {
+                                            proceed = true;
+                                            break;
+                                        }
+                                    }
+                                    if (proceed) {
+                                        char *name_untrimmed = copy_to(&whole[zz + 1], NULL, NULL, NULL);
+                                        char *type_untrimmed = (char *)malloc(zz + 1 + 1);
+                                        memcpy(type_untrimmed, cur, zz + 1);
+                                        type_untrimmed[zz + 1] = 0;
+                                        type = trim(type_untrimmed);
+                                        name = trim(name_untrimmed);
+                                        free(name_untrimmed);
+                                        free(type_untrimmed);
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                        if (type)
+                            break;
                     }
                     int name_len = name ? strlen(name) : 0;
                     if (delegate && parens) {
@@ -346,9 +487,11 @@ void main(int argc, char *argv[]) {
                         fprintf(stderr, "expected parens in override declaration\n");
                         exit_code(1);
                     }
-                    cur += name_len;
+                    cur += strlen(whole);
+                    free(whole);
                     bool has_semi = false;
                     char *trailing = copy_to(cur, ";", &has_semi, NULL);
+                    char *orig_trailing = trailing;
                     if (!has_semi) {
                         fprintf(stderr, "expected semi-colon\n");
                         exit_code(1);
@@ -370,15 +513,17 @@ void main(int argc, char *argv[]) {
                     }
                     for (trailing; isspace(*trailing); ++trailing) { }
                     if (*trailing != 0) {
+                        printf("trailing = %s, type = %s, name = %s\n", orig_trailing, type, name);
                         fprintf(stderr, "unexpected character '%c'\n", *trailing);
                         exit_code(1);
                     }
                     if (args) {
                         fprintf(fout, "\t%s(D,T,C,%s,%s,%s%s) %s\n",
-                            (override ? "override" : "method"), type, name, args, meta ? meta : "", e < (expr_count - 1) ? "\\" : "");
+                            (override ? "override" : (private ? "private_method" : "method")), type, name, args, meta ? meta : "", e < (expr_count - 1) ? "\\" : "");
                     } else {
                         fprintf(fout, "\t%s(D,T,C,%s,%s%s) %s\n",
-                            (delegate ? "object" : "var"), type, name, meta ? meta : "", e < (expr_count - 1) ? "\\" : "");
+                            (delegate ? "object" : (private ? "private_var" : "var")), type, 
+                                name, meta ? meta : "", e < (expr_count - 1) ? "\\" : "");
                     }
                     free(type);
                     free(name);
@@ -391,7 +536,7 @@ void main(int argc, char *argv[]) {
             fprintf(fout, "declare(%s,%s)", cname, *sname != 0 ? sname : (char *)"Base");
             free(cname);
             free(sname);
-        } else if (!new_line && token_read && strcmp(buf, "enum") == 0) {
+        } else if (!in_define && was_space && !new_line && token_read && strcmp(buf, "enum") == 0) {
             int len = strlen(start);
             int index = 0;
             char *ename = (char *)malloc(len + 1);
@@ -402,94 +547,96 @@ void main(int argc, char *argv[]) {
                 exit_code(1);
             }
             for (cur = &cur[index]; isspace(*cur); ++cur) { }
-            if (*cur != '{') {
-                fprintf(stderr, "expected { char to start enum block; found '%c'\n", *cur);
-                exit_code(1);
-            }
-            // read entire block
-            int expr_alloc = len + 1;
-            int expr_count = 0;
-            char **expr = (char **)malloc(expr_alloc * sizeof(char *));
-            memset(expr, 0, expr_alloc * sizeof(char *));
-            char *block = read_cblock(cur, &block_len, expr, &expr_count, ',', true);
-            block_len += (int)(size_t)(cur - start);
-            if (!block) {
-                fprintf(stderr, "invalid enum declaration block\n");
-                exit_code(1);
-            }
-            fprintf(fout, "#define _%s(D,T,C) _Enum(spr,T,C) \\\n", ename);
-            char *iname = (char *)malloc(len + 1);
-            iname[0] = 0;
-            int val = 0;
-            bool first = false;
-            bool allow_number = true;
-            int enum_cursor = 0;
-            int enum_inc = 1;
-            bool comma_given = true;
-            int enum_first = 0;
-            int enum_second = 1;
-            int num_specified_count = 0;
-
-            for (int e = 0; e < expr_count; e++) {
-                bool num_specified = false;
-                cur = expr[e];
-                for (cur; isspace(*cur); ++cur) { }
-                if (!comma_given) {
-                    fprintf(stderr, "comma expected in enum\n");
+            if (*cur == '{') {
+                // read entire block
+                int expr_alloc = len + 1;
+                int expr_count = 0;
+                char **expr = (char **)malloc(expr_alloc * sizeof(char *));
+                memset(expr, 0, expr_alloc * sizeof(char *));
+                char *block = read_cblock(cur, &block_len, expr, &expr_count, ',', true);
+                block_len += (int)(size_t)(cur - start);
+                if (!block) {
+                    fprintf(stderr, "invalid enum declaration block\n");
                     exit_code(1);
                 }
+                fprintf(fout, "#define _%s(D,T,C) _Enum(spr,T,C) \\\n", ename);
+                char *iname = (char *)malloc(len + 1);
                 iname[0] = 0;
-                val = 0;
-                if (sscanf(cur, "%[^=]=%d,%n", iname, &val, &index) != 2) {
-                    if (sscanf(cur, "%[^=]=%d%n", iname, &val, &index) != 2) {
-                        if (sscanf(cur, "%[^,],%n", iname, &index) != 1) {
-                            if (sscanf(cur, "%s%n", iname, &index) != 1) {
-                                fprintf(stderr, "expected enum definition\n");
-                                exit_code(1);
+                int val = 0;
+                bool first = false;
+                bool allow_number = true;
+                int enum_cursor = 0;
+                int enum_inc = 1;
+                bool comma_given = true;
+                int enum_first = 0;
+                int enum_second = 1;
+                int num_specified_count = 0;
+
+                for (int e = 0; e < expr_count; e++) {
+                    bool num_specified = false;
+                    cur = expr[e];
+                    for (cur; isspace(*cur); ++cur) { }
+                    if (!comma_given) {
+                        fprintf(stderr, "comma expected in enum\n");
+                        exit_code(1);
+                    }
+                    iname[0] = 0;
+                    val = 0;
+                    if (sscanf(cur, "%[^=]=%d,%n", iname, &val, &index) != 2) {
+                        if (sscanf(cur, "%[^=]=%d%n", iname, &val, &index) != 2) {
+                            if (sscanf(cur, "%[^,],%n", iname, &index) != 1) {
+                                if (sscanf(cur, "%s%n", iname, &index) != 1) {
+                                    fprintf(stderr, "expected enum definition\n");
+                                    exit_code(1);
+                                } else {
+                                    comma_given = false;
+                                    if (first)
+                                        allow_number = false;
+                                }
                             } else {
-                                comma_given = false;
+                                comma_given = true;
                                 if (first)
                                     allow_number = false;
                             }
                         } else {
-                            comma_given = true;
-                            if (first)
-                                allow_number = false;
+                            num_specified = true;
                         }
                     } else {
                         num_specified = true;
+                        comma_given = true;
                     }
-                } else {
-                    num_specified = true;
-                    comma_given = true;
-                }
-                sscanf(iname,"%s",iname);
-                if (num_specified && !allow_number) {
-                    fprintf(stderr, "number must be specified on first enum item\n");
-                    exit_code(1);
-                } else if (num_specified) {
-                    ++num_specified_count;
-                    if (num_specified_count == 1) {
-                        enum_first = val;
-                    } else if (num_specified_count == 2) {
-                        // infer auto-step
-                        enum_second = val;
-                        enum_inc = enum_second - enum_first;
-                        if (enum_inc == 0) {
-                            fprintf(stderr, "duplicate enum\n");
-                            exit_code(1);
+                    sscanf(iname,"%s",iname);
+                    if (num_specified && !allow_number) {
+                        fprintf(stderr, "number must be specified on first enum item\n");
+                        exit_code(1);
+                    } else if (num_specified) {
+                        ++num_specified_count;
+                        if (num_specified_count == 1) {
+                            enum_first = val;
+                        } else if (num_specified_count == 2) {
+                            // infer auto-step
+                            enum_second = val;
+                            enum_inc = enum_second - enum_first;
+                            if (enum_inc == 0) {
+                                fprintf(stderr, "duplicate enum\n");
+                                exit_code(1);
+                            }
                         }
+                        enum_cursor = val;
                     }
-                    enum_cursor = val;
+                    fprintf(fout, "\tenum_object(D,T,C,%s,%d) %s\n",
+                        iname, enum_cursor, e < (expr_count - 1) ? "\\" : "");
+                    enum_cursor += enum_inc;
                 }
-                fprintf(fout, "\tenum_object(D,T,C,%s,%d) %s\n",
-                    iname, enum_cursor, e < (expr_count - 1) ? "\\" : "");
-                enum_cursor += enum_inc;
+                fprintf(fout, "enum_declare(%s, Enum)", ename);
+            } else {
+                fprintf(fout, "%c", c);
             }
-            fprintf(fout, "enum_declare(%s, Enum)", ename);
         } else {
             fprintf(fout, "%c", c);
+            was_space = isspace(c);
         }
+        was_new_line = new_line;
     }
     free(in);
     free(buf);
--- obj/auto.h
+++ obj/auto.h
@@ -1,16 +1,12 @@
-#ifndef _AUTORELEASE_
-#define _AUTORELEASE_
-
-#define _AutoRelease(D,T,C) _Base(spr,T,C)         \
-    override(D,T,C,void,class_init,(Class))        \
-    override(D,T,C,void,init,(C))                  \
-    override(D,T,C,void,free,(C))                  \
-    method(D,T,C,AutoRelease,current,())    \
-    method(D,T,C,Base,add,(C,Base))         \
-    method(D,T,C,void,remove,(C,Base))      \
-    method(D,T,C,void,drain,(C))            \
-    private_var(D,T,C,LList,list)
-declare(AutoRelease, Base);
+class AutoRelease {
+    override void class_init(Class);
+    override void init(C);
+    override void free(C);
+    AutoRelease current();
+    Base add(C,Base);
+    void remove(C,Base);
+    void drain(C);
+    private LList list;
+}
 
 extern _thread_local_ AutoRelease *pool;
-#endif
