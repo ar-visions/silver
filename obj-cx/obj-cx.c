d1aee19b47621345809a9ce20f0d27527820f3ee
krworks <kyle@krworks.com>
2018-01-09 08:30:03 -0700

    Work on obj-cx

--- obj-cx/obj-cx.c
+++ obj-cx/obj-cx.c
@@ -1,13 +1,17 @@
 #include <obj/obj.h>
 #include <obj-cx/obj-cx.h>
 
+#define CX_VERSION  "0.4.0"
+
 implement(CX)
+implement(ClassDec)
+implement(MemberDec)
 
 Token *CX_read_tokens(CX self, String str, int *n_tokens) {
     Token *tokens = array_struct(Token, str->length + 1);
     int nt = 0;
-    char seps[256];
-    const char *separators = "[](){}.+-&*~!/%<>=^|&?:;#";
+    enum TokenType seps[256];
+    const char *separators = "[](){}.,+-&*~!/%<>=^|&?:;#";
     const char *puncts[] = {
         "[", "]", "(", ")", "{", "}", ".", "->",
         "++", "--", "&", "*", "+", "-", "~", "!",
@@ -46,8 +50,15 @@ Token *CX_read_tokens(CX self, String str, int *n_tokens) {
         keyword_lens[i] = strlen(keywords[i]);
     
     seps[0] = 1;
-    for (int i = 1; i <= 255; i++)
-        seps[i] = strchr(separators, i) != NULL;
+    for (int i = 1; i <= 255; i++) {
+        char *in_separators = strchr(separators, i);
+        if (strchr(separators, i) != NULL)
+            seps[i] = TT_Separator;
+        else if (i == '"' || i == '\'')
+            seps[i] = TT_String_Literal;
+        else
+            seps[i] = TT_None;
+    }
     const char backslash = '\\';
     bool was_backslash = false;
     Token *t = NULL;
@@ -55,17 +66,17 @@ Token *CX_read_tokens(CX self, String str, int *n_tokens) {
         const char *value = &str->buffer[i];
         const char b = *value;
         bool ws = isspace(b) || (b == 0);
-        bool sep = seps[b];
+        enum TokenType sep = seps[b];
 
         if (t && t->type == TT_String_Literal) {
-            if (t->string_term != b || was_backslash) {
+            if (t->string_term == b && !was_backslash) {
                 size_t length = (size_t)(value - t->value) + 1;
                 t->length = length;
                 was_backslash = b == backslash;
                 nt++;
                 t = NULL;
-                continue;
             }
+            continue;
         }
         if (ws) {
             if (t) {
@@ -73,9 +84,8 @@ Token *CX_read_tokens(CX self, String str, int *n_tokens) {
                 if (t->type == TT_Identifier) {
                     for (int k = 0; k < n_keywords; k++) {
                         const char *keyword = keywords[k];
-                        if (length == keyword_lens[k] && memcmp(t->value, keyword, length) == 0) {
+                        if (length == keyword_lens[k] && memcmp(t->value, keyword, length) == 0)
                             t->type = TT_Keyword;
-                        }
                     }
                 }
                 t->length = length;
@@ -83,6 +93,12 @@ Token *CX_read_tokens(CX self, String str, int *n_tokens) {
                 t = NULL;
             }
         } else {
+            if (t && sep != t->sep) {
+                size_t length = (size_t)(value - t->value);
+                t->length = length;
+                nt++;
+                t = NULL;
+            }
             if (!t) {
                 t = (Token *)&tokens[nt];
                 t->type = (b == '"' || b == '\'') ? TT_String_Literal : TT_Identifier;
@@ -90,6 +106,8 @@ Token *CX_read_tokens(CX self, String str, int *n_tokens) {
                 t->value = value;
                 t->length = 1;
                 t->string_term = (t->type == TT_String_Literal) ? b : 0;
+                if (t->string_term)
+                    continue;
             }
             size_t length = (size_t)(value - t->value) + 1;
             if (sep) {
@@ -107,9 +125,16 @@ Token *CX_read_tokens(CX self, String str, int *n_tokens) {
                     }
                 }
                 if (!cont) {
-                    t->length = length;
+                    t->length = length - 1;
                     nt++;
-                    t = NULL;
+                    t = (Token *)&tokens[nt];
+                    t->type = (b == '"' || b == '\'') ? TT_String_Literal : TT_Identifier;
+                    t->sep = sep;
+                    t->value = value;
+                    t->length = 1;
+                    t->string_term = (t->type == TT_String_Literal) ? b : 0;
+                    if (t->string_term)
+                        continue;
                 }
             }
         }
@@ -119,15 +144,93 @@ Token *CX_read_tokens(CX self, String str, int *n_tokens) {
     return tokens;
 }
 
+bool CX_read_template_types(CX self, ClassDec cd, int *cursor) {
+    Token *tokens = self->tokens;
+    if (tokens[*cursor].punct != "<")
+        return true;
+    // read template types
+    bool expect_identifier = true;
+    for (int t = tokens[*cursor]; t < n_tokens; t++) {
+        Token *tname = &tokens[t];
+        if (tname->punct == ">") {
+            if (!cd->templates) {
+                printf("expected template type\n");
+                exit(0);
+            }
+            break;
+        }
+        // expect identifier at even iterations
+        if (expect_identifier) {
+            if (tname->type == TT_Identifier) {
+                if (!cd->templates)
+                    cd->templates = new(List);
+                list_push(cd->templates, class_call(String, new_from_bytes, tname->value, tname->length));
+            } else {
+                printf("expected identifier in template expression\n");
+                exit(0);
+            }
+        } else {
+            if (tname->punct != ",") {
+                printf("expected ',' separator in template expression\n");
+                exit(0);
+            }
+        }
+        expect_identifier = !expect_identifier;
+        (*cursor)++;
+    }
+    return true;
+}
+
+inline void expect_type(Token token, enum TokenType type) {
+    if (token.type != type) {
+        printf("wrong token type\n");
+        exit(0);
+    }
+}
+
 bool CX_process(CX self, const char *file) {
     String str = class_call(String, from_file, file);
     int n_tokens = 0;
-    Token *tokens = call(self, read_tokens, str, &n_tokens);
+    self->tokens = call(self, read_tokens, str, &n_tokens);
+    Token *tokens = self->tokens;
+    // gather up all class declarations
     
-    // read tokens from str; store them on a List object
-}
+    printf("tokens:\n");
 
-#define CX_VERSION  "0.4.0"
+    // pass 1: collect classes
+    for (int i = 0; i < n_tokens; i++) {
+        Token *t = &tokens[i];
+        if (t->length == 7 && strncmp(t->value, "declare", t->length) == 0) {
+            ClassDec cd = new(ClassDec);
+            cd->name = &tokens[i + 1]; // validate
+            cd->methods = new(Pairs);
+            int cursor = i + 2;
+            call(self, read_template_types, cd, &cursor);
+            if (tokens[cursor++].punct != "{") {
+                printf("expected '{' character\n");
+                exit(0);
+            }
+
+            // read optional keywords such as static and private
+
+            // type
+            expect(token[cursor], TT_Identifier);
+            String str_type = class_call(String, new_from_bytes, token[cursor].value, token[cursor].length);
+            MemberDec member = new(MemberDec);
+            member->type = &token[cursor++];
+
+            // name
+            expect(token[cursor], TT_Identifier);
+            String str_name = class_call(String, new_from_bytes, token[cursor].value, token[cursor].length);
+            member->name = &token[cursor++];
+            pairs_add(cd->methods, str_name, member);
+
+            // expect meta ('[' or ';') for property, or ('[' or '(') for method
+        }
+    }
+
+    // read expressions, look for class declarations
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
--- obj-cx/obj-cx.h
+++ obj-cx/obj-cx.h
@@ -1,15 +1,22 @@
 
 enum TokenType {
-    TT_Keyword = 0,
+    TT_None = 0,
+    TT_Separator,
+    TT_Keyword,
     TT_Identifier,
     TT_Constant,
     TT_String_Literal,
     TT_Punctuator
 };
 
+enum MemberType {
+    MT_Prop = 0,
+    MT_Method
+};
+
 typedef struct _Token {
     enum TokenType type;
-    bool sep;
+    enum TokenType sep;
     const char *value;
     const char *punct;
     size_t length;
@@ -18,5 +25,24 @@ typedef struct _Token {
 
 #define _CX(D,T,C) _Base(spr,T,C)   \
     method(D,T,C,Token *,read_tokens,(C,String,int *)) \
-    method(D,T,C,bool,process,(C, const char *))
+    method(D,T,C,bool,read_template_types,(C,ClassDec, int *)) \
+    method(D,T,C,bool,process,(C, const char *)) \
+    var(D,T,C,Token *,tokens) \
 declare(CX, Base)
+
+#define _MemberDec(D,T,C) _Base(spr,T,C)   \
+    var(D,T,C,enum MemberType,member_type) \
+    var(D,T,C,Token *,type)                \
+    var(D,T,C,Token *,name)                \
+    var(D,T,C,bool,is_private)             \
+    var(D,T,C,bool,is_static)              \
+    var(D,T,C,Pairs,meta)                  \
+    var(D,T,C,Pairs,args)
+declare(MemberDec, Base)
+
+#define _ClassDec(D,T,C) _Base(spr,T,C)    \
+    var(D,T,C,Token *,name)                \
+    var(D,T,C,List,templates)              \
+    var(D,T,C,Pairs,members)               \
+    var(D,T,C,Pairs,meta)
+declare(ClassDec, Base)
