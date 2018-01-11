ad25e0782f642bfb0e8883c463abb2b8c989f3ee
krworks <kyle@krworks.com>
2018-01-10 23:38:28 -0700

    Work on class declaration parser

--- obj-cx/obj-cx.h
+++ obj-cx/obj-cx.h
@@ -19,6 +19,9 @@ typedef struct _Token {
     enum TokenType sep;
     const char *value;
     const char *punct;
+    const char *keyword;
+    const char *type_keyword;
+    bool operator;
     size_t length;
     char string_term;
 } Token;
@@ -28,6 +31,7 @@ struct _object_ClassDec;
 #define _CX(D,T,C) _Base(spr,T,C)   \
     method(D,T,C,Token *,read_tokens,(C,String,int *)) \
     method(D,T,C,bool,read_template_types,(C,struct _object_ClassDec *, Token **)) \
+    method(D,T,C,int,read_expression,(C, Token *, Token **, const char *)) \
     method(D,T,C,bool,process,(C, const char *)) \
     var(D,T,C,Token *,tokens)
 declare(CX, Base)
@@ -35,15 +39,22 @@ declare(CX, Base)
 #define _MemberDec(D,T,C) _Base(spr,T,C)   \
     var(D,T,C,enum MemberType,member_type) \
     var(D,T,C,Token *,type)                \
+    var(D,T,C,int,type_count)              \
+    var(D,T,C,String,str_name)             \
     var(D,T,C,Token *,name)                \
+    var(D,T,C,Token *,assign)              \
+    var(D,T,C,int,assign_count)            \
+    var(D,T,C,Token *,args)                \
+    var(D,T,C,int,args_count)              \
     var(D,T,C,bool,is_private)             \
     var(D,T,C,bool,is_static)              \
-    var(D,T,C,Pairs,meta)                  \
-    var(D,T,C,Pairs,args)
+    var(D,T,C,bool,is_const)               \
+    var(D,T,C,Pairs,meta)
 declare(MemberDec, Base)
 
 #define _ClassDec(D,T,C) _Base(spr,T,C)    \
     var(D,T,C,Token *,name)                \
+    var(D,T,C,String,super_class)          \
     var(D,T,C,List,templates)              \
     var(D,T,C,Pairs,members)               \
     var(D,T,C,Pairs,meta)
