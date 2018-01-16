a0c3669a845e0f76951f173cfe8c284cb4e015ca
krworks <kyle@krworks.com>
2018-01-16 09:10:51 -0700

    ...

--- obj-cx/obj-cx.h
+++ obj-cx/obj-cx.h
@@ -32,10 +32,10 @@ struct _object_ClassDec;
 #define _CX(D,T,C) _Base(spr,T,C)   \
     method(D,T,C,Token *,read_tokens,(C,String,int *)) \
     method(D,T,C,bool,read_template_types,(C,struct _object_ClassDec *, Token **)) \
-    method(D,T,C,int,read_expression,(C, Token *)) \
-    method(D,T,C,bool,read_declarations,(C)) \
-    method(D,T,C,bool,replace_declarations,(C)) \
-    method(D,T,C,bool,replace_definitions,(C)) \
+    method(D,T,C,void,replace_method_block,(C, List, Token *, Token *, Token **, int *)) \
+    method(D,T,C,int,read_expression,(C, Token *, Token **, Token **)) \
+    method(D,T,C,bool,read_classes,(C)) \
+    method(D,T,C,bool,replace_classes,(C)) \
     method(D,T,C,bool,replace_class_op,(C, Token *, Token *, \
         struct _object_ClassDec *, String)) \
     method(D,T,C,String,token_string,(C, Token *)) \
@@ -50,6 +50,8 @@ declare(CX, Base)
     var(D,T,C,Token *,type)                \
     var(D,T,C,int,type_count)              \
     var(D,T,C,String,str_name)             \
+    var(D,T,C,Token *,block_start)         \
+    var(D,T,C,Token *,block_end)           \
     var(D,T,C,Token *,name)                \
     var(D,T,C,Token *,assign)              \
     var(D,T,C,int,assign_count)            \
@@ -64,6 +66,8 @@ declare(MemberDec, Base)
 #define _ClassDec(D,T,C) _Base(spr,T,C)    \
     method(D,T,C,MemberDec,member_lookup,(C,String)) \
     var(D,T,C,C,parent)                    \
+    var(D,T,C,Token *,start)               \
+    var(D,T,C,Token *,end)                 \
     var(D,T,C,Token *,name)                \
     var(D,T,C,String,class_name)           \
     var(D,T,C,String,super_class)          \
--- test.c
+++ test.c
@@ -1,14 +1,88 @@
 
 
+// stack object initialization
+// keyword on method to indicate constructor method
+// be able to use constructor methods on stack or pointer allocations
+// from and to?
 
+// ** pool { blocks } for autorelease sections
+// ** try, throw, catch, and finally keywords
+// ** stack mode allocation for objects
+// ** add construct to structs with collection of members and their offset
+//         -> (offset will be difficult to know due to arbitrary compiler padding)
+//      allow operator overloading on structs
+//      implicit conversion of +=
+// ** serialize structs with introspection of constructors
+// ** while its not possible to get individual property members on structs due to alignment, you can serialize them one by one
 
-implement Vector <double, float> : Base {
+struct Test struct_Test_construct_int(Test *self, int p) {
+    // memset(self, 0, sizeof(Test)); <-- replace with simple stack Struct str = { };
+    // code output
+}
+
+// problem will be generating inline uses of structs such as 
+
+struct Test {
+    int a, b, c;
+
+    construct(int p) {
+        self.a = p;
+        self.b = p;
+        self.c = p;
+    }
+
+    // output as inlines to avoid copies; allow ability to be turned off to avoid code bloat
+    // pointers NOT allowed in operators
+
+    inline Test operator + (Test left, int right) {
+        return (Test) { left.x + right, left.y + right, left.z + right };
+    }
+
+    inline Test operator / (Test left, int right) {
+        return (Test) { left.a / right, left.b / right, left.c / right };
+    }
+
+    inline Test operator / (int left, Test right) {
+        return (Test) { left / right.a, left / right.b, left / right.c };
+    }
+};
+
+// allow stapling extra conversion methods on; such as this one to facilitate serialization of Test struct to String
+class String {
+    construct(Test *t) {
+        self.format("%f,%f,%f", t->x, t->y, t->z);
+    }
+}
+
+class Vector <double, float> {
+    construct(int i) {
+        // automatic construction of self; either stack or pointer
+        self->i = i;
+
+        // stack mode initialization
+        //      can NOT perform new struct(args)
+        //      -> will work with structs as well
+        //      -> operator overloads only on structs
+        //          due to the fact that pointer math must always remain
+
+        // constructors for type <-> type conversion
+
+        // pool blocks for fetching new pool and draining
+
+        // 
+        pool {
+
+        }
+
+        return self;
+    }
     private int test ^[key:value] {
         get {
             return self.that;
         }
     }
-    void method(int arg) {
+    String method(int arg) {
+        return (String)self; // <-- conversion method called; these conversion methods are a different animal than operator overloads in that they allow return
     }
 }
 
