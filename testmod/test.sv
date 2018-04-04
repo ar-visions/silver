ca4e1818cb7ca4722f970ac37575b9ce8cf8967d
Kyle White <kyle.white@guardvant.com>
2018-04-03 18:15:02 -0700

    ...

--- testmod/test.sv
+++ testmod/test.sv
@@ -1,25 +1,9 @@
 module supermod;
 
 class Test : Super {
-    construct () {
-        printf("default constructor\n");
-    }
-    construct i(int arg) {
-        self.value = arg;
-    }
-    construct s(String arg) {
-        self.value = 1;
-    }
-    int test1 = 1;
-    int value_intern;
-    static int value_test {
-        get {
-            return 1;
-        }
-        set (value) {
-            do_nothing;
-        }
-    }
+    
+    private int value_intern;
+
     int value {
         get {
             return self.value_intern;
@@ -28,11 +12,12 @@ class Test : Super {
             self.value_intern = value;
         }
     } = 2;
+
     void method(int arg, int arg2) {
         super.method(arg, arg2);
         printf("value = %d\n", self.value);
-        Test t = new Test();
     }
+
     static int Main() {
         Test t = new Test();
         t.method(1, 2);
