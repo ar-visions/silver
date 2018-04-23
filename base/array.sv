0f5c39f5fe748a2ea23b50cccb57ba8cfc6cf2cf
krworks <kyle@krworks.com>
2018-04-23 00:41:35 -0700

    start to implement templates; designed to emit code for primitives, and when used along with objects, emit code for Base and perform compile time type checks

--- base/array.sv
+++ base/array.sv
@@ -1,40 +1,41 @@
-class Array {
-    Base *buffer;
+class Array<T> {
+    T *buffer;
     int alloc_size;
     int count;
     
-    void resize() {
-        self.pop();
+    void *resize() {
         if (self.count + 1 > self.alloc_size) {
             int prev_size = self.alloc_size;
-            Base *prev = self.buffer;
+            T *prev = self.buffer;
             self.alloc_size += max(8, self.alloc_size << 1);
-            self.buffer = (Base *)malloc(self.alloc_size * sizeof(Base *));
+            self.buffer = (T *)malloc(self.alloc_size * sizeof(T));
             if (prev) {
                 if (prev_size > 0)
-                    memcpy(self.buffer, prev, prev_size * sizeof(Base *));
+                    memcpy(self.buffer, prev, prev_size * sizeof(T));
                 free(prev);
             }
         }
+        return self.buffer;
     }
-    void push(Base item) {
+    void push(T item) {
         self.resize();
         self.buffer[self.count] = item;
         self.count++;
     }
-    Base pop() {
+    T pop() {
         if (self.count == 0)
             return null;
-        Base item = self.buffer[--self.count];
+        T item = self.buffer[--self.count];
         self.buffer[self.count] = null;
         return item;
     }
-    int index_of(Base item) {
+    int index_of(T item) {
         for (int i = 0; i < self.count; i++) {
-            Base o = self.buffer[i];
+            T o = self.buffer[i];
             if (o == item)
                 return i;
         }
         return -1;
     }
 }
+
---
+++ base/primitives.sv
@@ -0,0 +1,41 @@
+class Integer {
+    int value;
+    long hash() {
+        return self.value;
+    }
+}
+
+class Long {
+    long value;
+    long hash() {
+        return self.value;
+    }
+}
+
+class Short {
+    short value;
+    long hash() {
+        return self.value;
+    }
+}
+
+class Char {
+    char value;
+    long hash() {
+        return self.value;
+    }
+}
+
+class Float {
+    float value;
+    long hash() {
+        return self.value;
+    }
+}
+
+class Double {
+    double value;
+    long hash() {
+        return self.value;
+    }
+}
